9a7c7d7624ea1cdd05249fc0a333d2a4764953ce
Marcelo Maia <mmaia.cc@gmail.com>
2020-01-03 21:57:24 -0300

    auhdsauhdsuahdsahud

--- utility/telegram_notification.sh
+++ utility/telegram_notification.sh
@@ -10,5 +10,5 @@ COMMIT_MSG=$(git log --format=%B -n 1)
 #     build_status="SUCCEEDED 🤙"
 # fi
 
-echo "Jenkins build #${env.BUILD_NUMBER} - ${env.JOB_NAME} - ${AUTHOR} - ${COMMIT_MSG} - ${env.JOB_DISPLAY_URL} ${BUILD_STATUS} - ${BRANCH_NAME}-  ${GIT_LOCAL_BRANCH} - ${GIT_BRANCH}"
+echo "Jenkins build #${BUILD_NUMBER} - ${JOB_NAME} - ${AUTHOR} - ${COMMIT_MSG} - ${JOB_DISPLAY_URL} ${BUILD_STATUS} - ${BRANCH_NAME}-  ${GIT_LOCAL_BRANCH} - ${GIT_BRANCH}"
 echo "XXXX ${GIT_COMMITTER_NAME} - ${GIT_AUTHOR_NAME} - ${GIT_COMMITTER_EMAIL} - ${GIT_AUTHOR_EMAIL}"
